b9e425e90abccf9464bf6c0b03e47fbadc72b684
reggin <mikhailo.gorishnyi@sandsiv.com>
2014-10-16 18:14:11 +0300

    Add rowFilter and readFilter. Add getExcel with filter

--- Xls/Reader/Reader.php
+++ Xls/Reader/Reader.php
@@ -3,41 +3,88 @@ namespace Arodiss\XlsBundle\Xls\Reader;
 
 use Arodiss\XlsBundle\Iterator\NestingDiscloseIterator;
 use Arodiss\XlsBundle\Iterator\StringifyIterator;
+use Arodiss\XlsBundle\Filter\ReadFilter;
+use Arodiss\XlsBundle\Filter\RowFilter;
 
-class Reader {
+class Reader
+{
 
-	/**
-	 * @param string $path
-	 * @return array
-	 */
-	public function readAll($path) {
-		return $this->getExcel($path)->toArray();
-	}
+    /**
+     * @param string $path
+     * @return array
+     */
+    public function readAll($path)
+    {
+        return $this->getExcel($path)->toArray();
+    }
 
-	/**
-	 * @param string $path
-	 * @return \Iterator
-	 */
-	public function getReadIterator($path) {
-		return new StringifyIterator(new NestingDiscloseIterator($this->getExcel($path)->getRowIterator()));
-	}
+    /**
+     * @param $path
+     * @param int $startRow
+     * @param int $size
+     * @return array
+     */
+    public function getByFilterAsArray($path, $startRow = 1, $size = 65000)
+    {
+        return RowFilter::clearEmptyRows($this->getPartOfExcel($path, $startRow, $size)->toArray());
+    }
 
-	/**
-	 * @param string $path
-	 * @return \Iterator
-	 */
-	public function getItemsCount($path) {
-		return $this->getExcel($path)->getHighestRow();
-	}
+    /**
+     * @param string $path
+     * @return \Iterator
+     */
+    public function getReadIterator($path)
+    {
+        return new StringifyIterator(new NestingDiscloseIterator($this->getExcel($path)->getRowIterator()));
+    }
 
-	/**
-	 * @param string $path
-	 * @return \PHPExcel_Worksheet
-	 */
-	protected function getExcel($path) {
-		$reader = \PHPExcel_IOFactory::createReaderForFile($path);
-		/** @var \PHPExcel $excel */
-		$excel = $reader->load($path);
-		return $excel->getActiveSheet();
-	}
+    /**
+     * @param string $path
+     * @return \Iterator
+     */
+    public function getItemsCount($path)
+    {
+        return $this->getExcel($path)->getHighestRow();
+    }
+
+    /**
+     * @param string $path
+     * @return \PHPExcel_Worksheet
+     */
+    protected function getExcel($path)
+    {
+        /** @var \PHPExcel $excel */
+        $excel = $this->getExcelWithoutSheet($path)->load($path);
+
+        return $excel->getActiveSheet();
+    }
+
+    /**
+     * @param $path
+     * @param $startRow
+     * @param $size
+     * @return \PHPExcel_Worksheet
+     */
+    protected function getPartOfExcel($path, $startRow, $size)
+    {
+        $reader = $this->getExcelWithoutSheet($path);
+        $reader->setReadFilter(new ReadFilter($startRow, $size));
+        /** @var \PHPExcel $excel */
+        $excel = $reader->load($path);
+
+        return $excel->getActiveSheet();
+    }
+
+    /**
+     * @param $path
+     * @return \PHPExcel_Reader_IReader
+     * @throws \PHPExcel_Reader_Exception
+     */
+    protected function getExcelWithoutSheet($path)
+    {
+        $reader = \PHPExcel_IOFactory::createReaderForFile($path);
+        $reader->setReadDataOnly(true);
+
+        return $reader;
+    }
 }
